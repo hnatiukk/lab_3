07a8ac00e97e6428b04b64a695e4bc72b5aadce0
Вова Гнатюк <vololdymyr.gn@gmail.com>
2022-10-08 23:15:32 +0300

    lab_3

---
+++ lab_3/lab_3/lab_3.cpp
@@ -0,0 +1,45 @@
+﻿//Калькулятор індексу маси тіла
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Введіть ваш зріст в сантиметрах = ";
+    int heightcm;
+    cin >> heightcm;
+
+    double heightm;
+    heightm = heightcm / 100.0;
+
+    cout << "Введіть вашу вагу в кілограмах = ";
+    int weight;
+    cin >> weight;
+
+    double imt;
+    imt = weight / pow(heightm, 2);
+
+    if (imt <= 18.5)
+    {
+        cout << "Ваш ІМТ = " << imt << " – вага недостатня.";
+    }
+
+    else if (imt > 18.5 && imt <= 24.9) 
+    {
+        cout << "Ваш ІМТ = " << imt << " – вага у нормі.";
+    }    
+
+    else if (imt >= 25 && imt <= 29.9) 
+    {
+        cout << "Ваш ІМТ = " << imt << " – надлишкова вага.";
+    }    
+    
+    else if (imt >= 30) 
+    {
+        cout << "Ваш ІМТ = " << imt << " – велика надмірна вага.";
+    }
+}
